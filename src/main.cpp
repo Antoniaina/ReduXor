aa4eeb87e5337643c82c796ce71a38c4fdde7331
Antoniaina <arisehenoantoniaina@gmail.com>
2025-10-21 23:49:10 +0300

    chore: initialize project structure

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "=== ReduXor: Boolean Simplifier ===\n\n";
+    
+    std::string expr;
+    std::cout << "Enter a boolean expression: ";
+    std::getline(std::cin, expr);
+
+    return 0;
+}
